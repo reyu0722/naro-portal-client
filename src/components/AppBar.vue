8d158d49184ef4cee8fc4b7e859f4b85dd776611
reyu0722 <ka15sugar@gmail.com>
2020-09-07 05:11:26 +0900

    replyを導入、AppBarほぼ完成

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
-    <v-navigation-drawer app v-model="drawer" dark clipped>
-      <v-list>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+    <v-navigation-drawer app v-model="drawer" clipped>
+      <v-list nav rounded>
+        <v-list-item-group v-model="item">
+          <router-link
+            v-for="item in items"
+            :key="item.title"
+            v-bind:to="item.link"
+            tag="v-list-item"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </router-link>
+        </v-list-item-group>
       </v-list>
       <template v-slot:append>
         <div class="pa-2">
@@ -18,6 +24,7 @@
         </div>
       </template>
     </v-navigation-drawer>
+
     <v-app-bar app color="primary" dark clipped-left>
       <div class="d-flex align-center">
         <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -41,11 +48,12 @@ export default {
   drawer: false,
   data() {
     return {
+      item: 1,
       items: [
-        { title: 'Home', icon: 'mdi-home-city' },
-        { title: 'About', icon: 'mdi-help-box' },
-        { title: 'My Account', icon: 'mdi-account' },
-        { title: 'Users', icon: 'mdi-account-group-outline' },
+        { title: 'Home', icon: 'mdi-home-city', link: '/' },
+        { title: 'About', icon: 'mdi-help-box', link: '/about' },
+        { title: 'My Account', icon: 'mdi-account', link: '/' },
+        { title: 'Users', icon: 'mdi-account-group-outline', link: '/' },
       ],
       drawer: null,
     }
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+import Vuetify from 'vuetify'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import { preset } from 'vue-cli-plugin-vuetify-preset-reply/preset'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({})
+export default new Vuetify({ preset })
